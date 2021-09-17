3983d3857a66fd77a7d23b90bbe2458e27e6dc7b
IddrisuAminu <iddrisuaminuict@gmail.com>
2021-09-17 12:03:14 +0000

    mobileappcalculator

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'packag:flutter/material.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp()
+}
+
+class Home extends statelesswidget{
+  class  extends StatelessWidget {
+    const ({ Key? key }) : super(key: key);
+  
+    @override
+    Widget build(BuildContext context) {
+      return container(
+
+        
+      ;body: column)
+      children;{text{"i am called Bed"}}
+
+      }
+
+    }
+  }
+}
